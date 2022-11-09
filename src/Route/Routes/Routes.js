5f9e7ff66a174ce392ed2ea02d210c1e6c4f21ef
Faridhosen4563 <faridhosen790@gmail.com>
2022-11-10 02:41:35 +0600

    add errorpage

--- src/Route/Routes/Routes.js
+++ src/Route/Routes/Routes.js
@@ -4,6 +4,7 @@ import AddService from "../../Pages/AddService/AddService";
 import AllServices from "../../Pages/AllServices/AllServices";
 import Blog from "../../Pages/Blog/Blog";
 import Details from "../../Pages/Details/Details";
+import Errorpage from "../../Pages/Errorpage/Errorpage";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
 import Register from "../../Pages/Login/Register/Register";
@@ -15,6 +16,7 @@ export const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+    errorElement: <Errorpage></Errorpage>,
     children: [
       {
         path: "/",
