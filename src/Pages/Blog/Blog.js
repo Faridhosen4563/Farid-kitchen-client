59f4d3b6bf71e78d512e131211742cd6d262ff5c
Faridhosen4563 <faridhosen790@gmail.com>
2022-12-21 19:20:04 +0600

    add some for ux

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -1,16 +1,31 @@
-import React from "react";
-import { useLoaderData } from "react-router-dom";
+import React, { useState } from "react";
+import { useEffect } from "react";
 import BlogItem from "./BlogItem";
 
 const Blog = () => {
-  const datas = useLoaderData();
+  const [totalData, setTotalData] = useState([]);
+  const [spinner, setSpinner] = useState(true);
+
+  useEffect(() => {
+    fetch("https://farid-kitchen-server-site.vercel.app/blog")
+      .then((res) => res.json())
+      .then((data) => {
+        setTotalData(data);
+        setSpinner(false);
+      });
+  }, []);
+
+  if (spinner) {
+    return <progress className="progress w-full"></progress>;
+  }
+
   return (
     <div className="mb-20">
       <h2 className="text-2xl text-center font-medium my-6">
         Welcome to Blog Part
       </h2>
       <div>
-        {datas.map((data) => (
+        {totalData.map((data) => (
           <BlogItem key={data._id} data={data}></BlogItem>
         ))}
       </div>
